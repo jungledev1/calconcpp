3b053781ce694df50066724714efd61782674daa
timka123 <vakzncode@yandex.ru>
2022-01-08 15:29:51 +0300

    first

---
+++ cmake-build-debug/CMakeFiles/cppHello.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppHello.dir/main.cpp.obj"
+  "cppHello.exe"
+  "cppHello.exe.manifest"
+  "cppHello.pdb"
+  "libcppHello.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppHello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
