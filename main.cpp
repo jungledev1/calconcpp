cd0cedacc173a281152c44ddd475dbbc4e428341
Jungle <vakzncode@yandex.ru>
2022-01-08 15:49:23 +0300

    add work code

--- main.cpp
+++ main.cpp
@@ -3,6 +3,29 @@
 using namespace std;
 
 int main() {
-    cout << "Hello world" << endl;
+    int num_1, num_2, result;
+    char work;
+
+    cin >> num_1;
+    cin >> work;
+    cin >> num_2;
+    if (work == '+') {
+        result = num_1 + num_2;
+        cout << result << endl;
+    }
+    if (work == '-') {
+        result = num_1 - num_2;
+        cout << result << endl;
+    }
+    if (work == '*') {
+        result = num_1 * num_2;
+        cout << result << endl;
+    }
+    if (work == '/') {
+        result = num_1 / num_2;
+        cout << result << endl;
+    }
+
+
     return 0;
 }
